301657bd2e4cd96becd53cee92ee282e5cdfa2bc
Akira Hayakawa <ruby.wktk@gmail.com>
2024-06-01 20:55:22 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- sled/src/main.rs
+++ sled/src/main.rs
@@ -1,7 +1,5 @@
 use clap::Parser;
-use crossbeam::queue::SegQueue;
-use std::sync::Arc;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
 #[derive(Parser)]
 struct Opts {
@@ -12,7 +10,7 @@ struct Opts {
 }
 fn main() {
     let opts = Opts::parse();
-    let (collector, q) = Collector::new();
+    let (collector, q) = stats::Collector::new();
 
     std::fs::remove_dir_all("db").ok();
     std::fs::create_dir("db").unwrap();
@@ -20,10 +18,10 @@ fn main() {
 
     for lane_id in 0..opts.n_lanes {
         let tree = db.open_tree(format!("id={lane_id}")).unwrap();
-        let mut reporter = Reporter::new(q.clone());
+        let mut reporter = stats::Reporter::new(q.clone());
         std::thread::spawn(move || for i in 0.. {
             let k = i.to_string();
-            let v = randbytes(opts.datalen);
+            let v = stats::randbytes(opts.datalen);
 
             reporter.start();
             tree.insert(&k, v).unwrap();
@@ -36,68 +34,3 @@ fn main() {
     eprintln!("{}", collector.show(du));
 }
 
-pub fn randbytes(n: usize) -> Vec<u8> {
-    vec![0; n]
-}
-
-pub type Queue = Arc<SegQueue<Packet>>;
-
-pub struct Packet {
-    pub time: Duration,
-    pub length: usize,
-}
-
-pub struct Collector {
-    q: Queue,
-}
-impl Collector {
-    pub fn new() -> (Self, Queue) {
-        let q = Arc::new(SegQueue::new());
-        let this = Self { q: q.clone() };
-        (this, q)
-    }
-
-    pub fn show(self, du: Duration) -> String {
-        let mut n = 0;
-        let mut sum_time = Duration::ZERO;
-        let mut sum_amt = 0; // bytes
-        let n = self.q.len();
-        let mut remaining = n;
-        while remaining > 0 {
-            let packet = self.q.pop().unwrap();
-            sum_amt += packet.length;
-            sum_time += packet.time;
-            remaining -= 1;
-        }
-
-        let latency = sum_time / n as u32;
-        // MB/s
-        let throughput = (sum_amt as f64 / du.as_millis() as f64) / 1000.;
-        format!("throughput {throughput} MB/s, latency {latency:?}")
-    }
-}
-
-pub struct Reporter {
-    q: Queue,
-    cur: Option<Instant>,
-}
-impl Reporter {
-    pub fn new(q: Queue) -> Self {
-        Self { q, cur: None }
-    }
-
-    pub fn start(&mut self) {
-        assert!(self.cur.is_none());
-        let now = Instant::now();
-        self.cur = Some(now);
-    }
-
-    pub fn stop(&mut self, n: usize) {
-        let old = self.cur.take().unwrap();
-        let elapsed = old.elapsed();
-        self.q.push(Packet {
-            time: elapsed,
-            length: n,
-        })
-    }
-}
---
+++ stats/src/lib.rs
@@ -0,0 +1,72 @@
+use crossbeam::queue::SegQueue;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+
+pub fn randbytes(n: usize) -> Vec<u8> {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    let mut out = vec![0; n];
+    rng.fill(&mut out[..]);
+    out
+}
+
+pub type Queue = Arc<SegQueue<Packet>>;
+
+pub struct Packet {
+    time: Duration,
+    length: usize,
+}
+
+pub struct Collector {
+    q: Queue,
+}
+impl Collector {
+    pub fn new() -> (Self, Queue) {
+        let q = Arc::new(SegQueue::new());
+        let this = Self { q: q.clone() };
+        (this, q)
+    }
+
+    pub fn show(self, du: Duration) -> String {
+        let mut sum_time = Duration::ZERO;
+        let mut sum_amt = 0; // bytes
+        let n = self.q.len();
+        let mut remaining = n;
+        while remaining > 0 {
+            let packet = self.q.pop().unwrap();
+            sum_amt += packet.length;
+            sum_time += packet.time;
+            remaining -= 1;
+        }
+
+        let latency = sum_time / n as u32;
+        // MB/s
+        let throughput = (sum_amt as f64 / du.as_millis() as f64) / 1000.;
+        format!("throughput={throughput} MB/s, latency={latency:?}")
+    }
+}
+
+pub struct Reporter {
+    q: Queue,
+    cur: Option<Instant>,
+}
+impl Reporter {
+    pub fn new(q: Queue) -> Self {
+        Self { q, cur: None }
+    }
+
+    pub fn start(&mut self) {
+        assert!(self.cur.is_none());
+        let now = Instant::now();
+        self.cur = Some(now);
+    }
+
+    pub fn stop(&mut self, n: usize) {
+        let old = self.cur.take().unwrap();
+        let elapsed = old.elapsed();
+        self.q.push(Packet {
+            time: elapsed,
+            length: n,
+        })
+    }
+}
