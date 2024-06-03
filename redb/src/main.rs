682b4f850abe7e5c85a2b5e1eca03936eadfc5bf
Akira Hayakawa <ruby.wktk@gmail.com>
2024-06-03 19:29:12 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- redb/src/main.rs
+++ redb/src/main.rs
@@ -1,6 +1,40 @@
 use clap::Parser;
 use std::sync::Arc;
-use std::time::Duration;
+use std::time::{Instant, Duration};
+use std::sync::mpsc;
+use anyhow::Result;
+
+struct LazyInsert {
+    index: u64,
+    bin: Vec<u8>,
+    space: String,
+    notifier: oneshot::Sender<()>,
+}
+struct Reaper {
+    db: Arc<redb::Database>,
+    recv: mpsc::Receiver<LazyInsert>,
+}
+impl Reaper {
+    fn table_def(space: &str) -> redb::TableDefinition<u64, Vec<u8>> {
+        redb::TableDefinition::new(space)
+    }
+    fn reap(&self, du: Duration) -> Result<()> {
+        let deadline = Instant::now() + du;
+        let mut notifiers = vec![];
+        let tx = self.db.begin_write()?;
+        while let Ok(e) = self.recv.recv_timeout(deadline - Instant::now()) {
+            let mut tbl = tx.open_table(Self::table_def(&e.space))?;
+            tbl.insert(e.index, e.bin)?;
+            notifiers.push(e.notifier);
+        }
+        tx.commit()?;
+
+        for notifier in notifiers {
+            notifier.send(()).unwrap();
+        }
+        Ok(())
+    }
+}
 
 #[derive(Parser)]
 struct Opts {
@@ -16,13 +50,21 @@ fn main() {
     std::fs::remove_file("db").ok();
     let db = Arc::new(redb::Database::create("db").unwrap());
     
-    let mut tx = db.begin_write().unwrap();
-    tx.set_durability(redb::Durability::Eventual);
-    tx.commit().unwrap();
+    let (tx, rx) = mpsc::channel();
+    let reaper = Reaper {
+        db: db.clone(),
+        recv: rx,
+    };
+    std::thread::spawn(move || {
+        loop {
+            reaper.reap(Duration::from_millis(100)).ok();
+        }
+    });
 
     for lane_id in 0..opts.n_lanes {
         let db = db.clone();
         let mut reporter = stats::Reporter::new(q.clone());
+        let tx = tx.clone();
 
         std::thread::spawn(move || {
             let tblname = format!("id={lane_id}");
@@ -32,12 +74,26 @@ fn main() {
                 let v = stats::randbytes(opts.datalen);
 
                 reporter.start();
-                let tx = db.begin_write().unwrap();
-                {
-                    let mut tbl = tx.open_table(tbldef).unwrap();
-                    tbl.insert(k, v).unwrap();
+
+                if false {
+                    let tx = db.begin_write().unwrap();
+                    {
+                        let mut tbl = tx.open_table(tbldef).unwrap();
+                        tbl.insert(k, v).unwrap();
+                    }
+                    tx.commit().unwrap();
+                } else {
+                    let (tx1, rx1) = oneshot::channel();
+                    let e = LazyInsert {
+                        index: k,
+                        bin: v,
+                        space: tblname.clone(),
+                        notifier: tx1,
+                    };
+                    tx.send(e).unwrap();
+                    rx1.recv().unwrap();
                 }
-                tx.commit().unwrap();
+
                 reporter.stop(opts.datalen);
             }
         });
